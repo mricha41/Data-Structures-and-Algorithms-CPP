055355ed44dc704cdbc28bd527d4bafd4b1a2a9f
mricha41 <mark@markrichards.ninja>
2020-12-09 12:36:30 -0500

    fixed Stack display function, it was displaying from the bottom to the top of the stack, now it displays from the top to the bottom of the stack as you would expect

--- Stack/include/Algorithms.hpp
+++ Stack/include/Algorithms.hpp
@@ -9,7 +9,7 @@ void Display(Stack<stackSize, T1> stack)
     std::cout << "Stack size: " << stack.size() << std::endl;
     std::cout << "Stack max size: " << stack.maxSize() << std::endl;
     std::cout << "Elements in Stack: " << std::endl;
-    for (std::size_t i = 0; i < stack.size(); ++i)
+    for (std::size_t i = stack.size() - 1; i > 0; --i)
     {
         i < stack.size() ? std::cout << stack[i] << " " :
             std::cout << stack[i];
